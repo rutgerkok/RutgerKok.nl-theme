e7db2b0d2e89c50cdb30a718989c74cc74513885
Rutger Kok <rutger.kok@live.com>
2019-10-26 20:18:36 +0200

    Adjust margins, meta text and links

--- header.php
+++ header.php
@@ -28,15 +28,9 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
+			?>
+			<div class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php
 			$rutgerkok_description = get_bloginfo( 'description', 'display' );
 			if ( $rutgerkok_description || is_customize_preview() ) :
 				?>
