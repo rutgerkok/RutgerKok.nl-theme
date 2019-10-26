d32a33872c7d5c278aa1d7d6d99977189181f8ee
Rutger Kok <rutger.kok@live.com>
2019-10-26 21:44:18 +0200

    Display menu even on small screens
    Now the navigation JS can be removed.

--- functions.php
+++ functions.php
@@ -122,8 +122,6 @@ add_action( 'widgets_init', 'rutgerkok_widgets_init' );
 function rutgerkok_scripts() {
 	wp_enqueue_style( 'rutgerkok-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'rutgerkok-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( 'rutgerkok-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
--- header.php
+++ header.php
@@ -39,7 +39,6 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'rutgerkok' ); ?></button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
